61c63b682c335eab914c97b420c2b7879fbcd272
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 17:51:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>HELLO THERE</h1> /n <h3>you are viewing my website</h3>"
+    return "<h1>HELLO THERE</h1> <h3>you are viewing my website</h3>"
+
+@app.route('hi')
+def hi():
+    return "hello"
     
 @app.route("/<name>")
 def user(name):
     return f"<h1>error</h1> you silly goober, the page {name} probably doesn't exist (or the creator did a dum)"
-    
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+
