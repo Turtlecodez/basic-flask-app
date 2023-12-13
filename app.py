3cd7bfc78e70328b53dd3e69d0dab849efa9ddf3
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 17:41:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, world!"
-
+@app.route("/<name>")
+def user(name):
+    return f"you went to page {name}"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
