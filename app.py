5c40083e87f61090f4ba1d6a172fc4b2c24e727e
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 17:43:39 -0500

    Update app.py
     m o r e  c o d i n g

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Hello, world!"
+    
 @app.route("/<name>")
 def user(name):
-    return f"you went to page {name}"
+    return f"you silly goober, the page /"{name}/" doesn't exist lol"
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
