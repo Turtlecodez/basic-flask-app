cd5f5c6d7ee630e75f5ac2ab8dede366c92dd698
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 18:01:43 -0500

    idk

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 import os
 
 app = Flask(__name__)
@@ -7,11 +7,11 @@ app = Flask(__name__)
 def index():
     return "<h1>HELLO THERE</h1> <h3>you are viewing my website</h3>"
     
-@app.route("/<name>")
+@app.route("/<name>/")
 def user(name):
     return f"<h1>error</h1> you silly goober, the page {name} probably doesn't exist (or the creator did a dum)"
 
-@app.route("/admin")
+@app.route("/admin/")
 def admin():
     return "youre not an admin >:("
 
