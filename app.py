bd7dd90de03754758f80888541345f9604e6c6c3
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 17:46:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     
 @app.route("/<name>")
 def user(name):
-    return f"you silly goober, the page /"{name}/" doesn't exist lol"
+    return f"you silly goober, the page {name} doesn't exist lol"
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
