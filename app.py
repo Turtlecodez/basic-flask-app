d437b1d918a634d607da0a772944c8085ec0b2f9
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 17:58:49 -0500

    youre not an admin

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
     return "<h1>HELLO THERE</h1> <h3>you are viewing my website</h3>"
     
 @app.route("/<name>")
 def user(name):
     return f"<h1>error</h1> you silly goober, the page {name} probably doesn't exist (or the creator did a dum)"
+
+@app.route("/admin")
+def admin():
+    return "youre not an admin >:("
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
