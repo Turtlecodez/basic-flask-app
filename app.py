97e8e0164e69a9e73186a2dafe3c56d83720374d
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-13 18:20:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("something.html")
     
 @app.route("/<name>/")
 def user(name):
