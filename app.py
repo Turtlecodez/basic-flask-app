e35f6d0e4dcec22b39be56c24d04ab07d8656d31
Turtlecodez <118611581+Turtlecodez@users.noreply.github.com>
2023-12-14 09:09:53 -0500

    fixing :(

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
+def index():
+    return "<h1>HELLO THERE</h1> <h3>you are viewing my website</h3>"
 
-@app.route("/")
-def home():
-    return render_template("something.html")
-    
 @app.route("/<name>/")
 def user(name):
     return f"<h1>error</h1> you silly goober, the page {name} probably doesn't exist (or the creator did a dum)"
